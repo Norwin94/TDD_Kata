41e82a1bf53cad4f31c69850e7e923473a23dc26
Łukasz Szumilas <lukasz.szumilas1994@gmail.com>
2019-06-29 19:45:45 +0200

    0 - add Java Project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TDD_Kata
 Calculator task
+
+Simple calculator to add numbers, task from https://osherove.com/tdd-kata-1
---
+++ src/Calc.java
@@ -0,0 +1,9 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calc {
+	public static void main(String[] args) {
+		
+	
+	}
+}
